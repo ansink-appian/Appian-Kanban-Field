07581f02ab279bd94a276a37aeb9e97e9c8d5766
mark.ansink <mark.ansink@appian.com>
2023-04-24 11:35:44 +0200

    Added info banner

---
+++ kanbanField/v1/__shared/README.md
@@ -0,0 +1,6 @@
+# Shared Folder
+
+## Introduction
+
+> Any files/folders added here will be copied to the LATEST version of each component upon exporting this package using `sail-tools`. 
+The directory structure will be maintained, and any files will be referencable via `__shared/YOUR_FILES`.
--- kanbanField/v1/js/kanban.js
+++ kanbanField/v1/js/kanban.js
@@ -2,49 +2,10 @@ let locale = Appian.getLocale(); //Returns the user's locale (eg. en-US). This a
 let accentColor = Appian.getAccentColor(); //Returns the accent color of the Appian environment in hexadecimal format
 let inputTasks;
 let inputColumns;
-let defaultTasks = [{
-    "id": 1,
-    "title": "Design website layout",
-    "description": "Create wireframes and mockups for the website layout",
-    "status": "ToDo",
-    "priority": "High",
-    "assignee": "mark.ansink@appian.com",
-    "dueDate": "04 April 2023",
-}, {
-    "id": 2,
-    "title": "Develop homepage",
-    "description": "Code the HTML, CSS, and JavaScript for the homepage",
-    "status": "Active",
-    "assignee": "mark.ansink@appian.com",
-    "priority": "Low",
-    "assignee": "mark.ansink@appian.com",
-    "dueDate": "04 April 2023",
-}, {
-    "id": 3,
-    "title": "Implement backend functionality",
-    "description": "Create APIs and database connections for the website",
-    "status": "Active",
-    "assignee": "mark.ansink@appian.com",
-    "priority": "Medium",
-    "assignee": "mark.ansink@appian.com",
-    "dueDate": "04 April 2023",
-}, {
-    "id": 4,
-    "title": "Test website",
-    "description": "Perform manual and automated testing to ensure the website works as expected",
-    "status": "Done",
-    "assignee": "mark.ansink@appian.com",
-    "priority": "Low",
-    "assignee": "mark.ansink@appian.com",
-    "dueDate": "04 April 2023",
-}];
-let defaultColumns = ["ToDo", "Active", "Done"];
-let columns;
-let tasks;
 let board = document.querySelector(".board");
 let selectedCard = null;
 
-function defaultValue(string){
+function defaultValue(string) {
     const value = string ?? '-';
     return value
 };
@@ -65,6 +26,31 @@ let lighterColorCode = "#" +
     green.toString(16).padStart(2, "0") +
     blue.toString(16).padStart(2, "0");
 
+
+function bannerInformation(info) {
+
+    console.log("Dit is een test: "+info);
+    const bannerElement = document.createElement("div");
+    bannerElement.className = "kfBannerContainer";
+    bannerElement.innerHTML = `<div class="kfBanner">
+              <div class="kfSideLine" background-color="${accentColor}">
+            
+                </div>
+                <div class="kfInfoSvg">
+                  <svg focusable="false" preserveAspectRatio="xMidYMid meet" xmlns="http://www.w3.org/2000/svg" fill="currentColor" width="18" height="18" viewBox="0 0 32 32" aria-hidden="true" style="
+              fill: ${accentColor};
+            "><path fill="none" d="M16,8a1.5,1.5,0,1,1-1.5,1.5A1.5,1.5,0,0,1,16,8Zm4,13.875H17.125v-8H13v2.25h1.875v5.75H12v2.25h8Z"></path><path d="M16,2A14,14,0,1,0,30,16,14,14,0,0,0,16,2Zm0,6a1.5,1.5,0,1,1-1.5,1.5A1.5,1.5,0,0,1,16,8Zm4,16.125H12v-2.25h2.875v-5.75H13v-2.25h4.125v8H20Z"></path><title>Information filled</title></svg>
+                </div>
+                <div class="infoCont">
+                   <div class="informationAp">
+                 Information Notification
+                </div>
+                <div>
+                   No ${info} configured or available. 
+                </div>
+                </div
+              </div>`
+}
 Appian.Component.onNewValue(function (allParameters) { // Whenever a new value is provided to ANY input, this function is invoked with ALL inputs as a dictionary. This includes when the component is initialized.
     inputTasks = allParameters['tasks'];
     inputColumns = allParameters['columns'];
@@ -73,60 +59,57 @@ Appian.Component.onNewValue(function (allParameters) { // Whenever a new value i
 
 
 
-
-    // Add Columns
+    console.log(inputTasks  + " tasks: "+inputColumns);
+// Add Columns
     if (!inputColumns) {
-        columns = defaultColumns;
+        bannerInformation("Column");
     } else {
-        columns = inputColumns;
-    }
-
-    board.innerHTML = '';
-    columns.forEach((column) => {
-
-        const listElement = document.createElement("div");
-        listElement.className = "tasks";
-        listElement.draggable = false;
-        const columnName = column.toUpperCase();
-        listElement.innerHTML = `<h5 class="mt-0 task-header" id="${column}-header">${columnName} (0)</h5>
+        board.innerHTML = '';
+        inputColumns.forEach((column) => {
+            const listElement = document.createElement("div");
+            listElement.className = "tasks";
+            listElement.draggable = false;
+            const columnName = column.toUpperCase();
+            listElement.innerHTML = `<h5 class="mt-0 task-header" id="${column}-header">${columnName} (0)</h5>
               <div id="${column}" name="${column}" class="task-list-items">`
-        board.appendChild(listElement);
-
-    });
-
+            board.appendChild(listElement);
+        });
+    }
 // Add Tasks
     let draggingCard = null;
     const taskLists = document.querySelectorAll(".task-list-items");
 
     if (!inputTasks) {
-        tasks = defaultTasks
+        bannerInformation("Tasks");
+
     } else {
-        tasks = inputTasks
-    }
 
 
-    tasks.forEach((card) => {
-        let divCard = document.getElementById(card.status);
+        inputTasks.forEach((card) => {
+            let divCard = document.getElementById(card.status);
 
-        // If Column is not found do nothing,
-        // TODO Maybe add validation here
-        if (divCard===null){return};
+            // If Column is not found do nothing,
+
+            if (divCard === null) {
+                return
+            }
+            ;
 
 
-        const cardElement = document.createElement("div");
-        cardElement.className = "card mb-0";
-        cardElement.draggable = true;
-        cardElement.id = card.id;
-        cardElement.style.borderLeftColor = accentColor;
-        if (card.id===selectedCard){
-            cardElement.style.backgroundColor=lighterColorCode;
-        }
-        addCardClickListener(cardElement);
-        cardElement.addEventListener("dragend", () => {
+            const cardElement = document.createElement("div");
+            cardElement.className = "card mb-0";
+            cardElement.draggable = true;
+            cardElement.id = card.id;
+            cardElement.style.borderLeftColor = accentColor;
+            if (card.id === selectedCard) {
+                cardElement.style.backgroundColor = lighterColorCode;
+            }
+            addCardClickListener(cardElement);
+            cardElement.addEventListener("dragend", () => {
 
-        });
+            });
 
-        cardElement.innerHTML = `
+            cardElement.innerHTML = `
         <div class="card-body p-3" id="${card.id}">
         
           <small class="float-end text-muted card-date">
@@ -136,14 +119,14 @@ Appian.Component.onNewValue(function (allParameters) { // Whenever a new value i
 </svg>${defaultValue(card.dueDate)}</small>
           <span class="badge 
           ${
-            card.priority === "High"
-                ? "bg-danger"
-                : card.priority === "Medium"
-                    ? "bg-warning"
-                    : card.priority === "Low"
-                        ? "bg-success"
-                        : ""
-        }
+                card.priority === "High"
+                    ? "bg-danger"
+                    : card.priority === "Medium"
+                        ? "bg-warning"
+                        : card.priority === "Low"
+                            ? "bg-success"
+                            : ""
+            }
           text-light">${card.priority}</span>
           <h5 class="mt-2 card-title text-secondary">${defaultValue(card.title)}</h5>
         
@@ -162,19 +145,20 @@ Appian.Component.onNewValue(function (allParameters) { // Whenever a new value i
         </div>
       `;
 
-        // Add event listener for dragstart on created card element
-        cardElement.addEventListener("dragstart", function (event) {
-            draggingCard = event.target;
-            event.dataTransfer.setData("text/plain", event.target.id);
-            event.target.classList.add("dragging");
+            // Add event listener for dragstart on created card element
+            cardElement.addEventListener("dragstart", function (event) {
+                draggingCard = event.target;
+                event.dataTransfer.setData("text/plain", event.target.id);
+                event.target.classList.add("dragging");
 
-        });
+            });
 
-        document.getElementById(card.status).appendChild(cardElement);
-    });
+            document.getElementById(card.status).appendChild(cardElement);
+        })
+
 
     function updateTaskStatus(id, newStatus) {
-        let task =tasks.find(t => t.id === parseInt(id));
+        let task = inputTasks.find(t => t.id === parseInt(id));
         if (task) {
             task.status = newStatus;
         }
@@ -194,7 +178,7 @@ Appian.Component.onNewValue(function (allParameters) { // Whenever a new value i
             const cardId = event.dataTransfer.getData("text/plain");
             const column = event.target.closest(".task-list-items");
             updateTaskStatus(cardId, column.id);
-            Appian.Component.saveValue("tasks", tasks)
+            Appian.Component.saveValue("tasks", inputTasks)
             if (draggingCard !== null) {
                 column.appendChild(draggingCard);
                 draggingCard = null;
@@ -225,19 +209,20 @@ Appian.Component.onNewValue(function (allParameters) { // Whenever a new value i
             updateTaskCount();
         });
     });
-
+    }
+    ;
     updateTaskCount();
+
     function addCardClickListener(cardElement) {
         cardElement.addEventListener('click', (event) => {
 
-            if (selectedCard===parseInt(cardElement.id)) {
-                cardElement.style.backgroundColor="white";
-                selectedCard=null;
+            if (selectedCard === parseInt(cardElement.id)) {
+                cardElement.style.backgroundColor = "white";
+                selectedCard = null;
                 Appian.Component.saveValue("selectedTask", null);
-            }
-            else {
+            } else {
                 selectedCard = cardElement.id;
-                cardElement.style.backgroundColor=lighterColorCode;
+                cardElement.style.backgroundColor = lighterColorCode;
                 Appian.Component.saveValue("selectedTask", cardElement.id);
             }
         });
